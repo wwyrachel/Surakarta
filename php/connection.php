a3f04ac24fb8fd70b531154654ff0f55bda5e9d9
wwyrachel <49199823+wwyrachel@users.noreply.github.com>
2019-05-18 16:34:49 +0800

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,8 +1,8 @@
 <?php
 
 $user="root";
-$password="//your db password";
-$dbname="a2";
+$password="**your db password**";
+$dbname="**db name**";
 
 $con= new mysqli("localhost", $user, $password, $dbname) or die("unable to open");
 //connect to the database
